c3afdde385226a9080deea7ea2ab7f761c6bdd59
knifer0 <Wojtek_430@interia.pl>
2019-09-17 17:39:20 +0200

    Create ConsoleApplication23.cpp

---
+++ rozdzial_13/4/ConsoleApplication23.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "port.h"
+int main() {
+	using std::cin;
+	using std::cout;
+	using std::endl;
+	using std::string;
+	
+	VintagePort zmienna("Galilieo", "zielony", 33, "Bula", 1998);
+	Port zmienna3("Gallo", "ledkko brazowy", 20);
+	Port* wsk = &zmienna;
+	zmienna3 = zmienna;
+	cout << *wsk;
+}
