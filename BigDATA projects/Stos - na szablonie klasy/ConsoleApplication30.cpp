d853685a7b67ca3a01dd9e12f3bdf841a2cf68ba
knifer0 <Wojtek_430@interia.pl>
2019-09-18 17:23:12 +0200

    Create ConsoleApplication30.cpp

---
+++ BigDATA projects/Stos - na szablonie klasy/ConsoleApplication30.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include "stos.h"
+#include <cstdlib>
+
+int main() {
+	using std::cout;
+	using std::endl;
+	using std::cin;
+	Stos<int>* p1 = new Stos <int>;
+	char choice{};
+	int number{};
+	while (1) {
+		linia();
+		cout << "1. Dodaj na stos" << endl; // push
+		cout << "2. Zdemij ze stosu" << endl; // pop
+		cout << "3. Sprawdz stos" << endl;
+		linia();
+		cin >> choice;
+		while (cin.get() != '\n')
+			continue;
+		switch (choice) {
+		case '1':
+			if (!p1->isfull()) {
+				cout << "Podaj liczbe: ";
+				while (!(cin >> number)) {
+					cin.clear();
+					while (cin.get() != '\n')
+						continue;
+				}
+				p1->push(number);
+				while (cin.get() != '\n')
+					continue;
+			}
+			break;
+
+		case '2':
+			if (!p1->isempty())
+				p1->pop(number);
+			break;
+
+		case '3':
+			linia();
+			p1->print();
+			break;
+		case EOF:
+			exit(1);
+		default:
+			std::cout << "Nieprawidlowa operacja" << endl;
+			exit(EXIT_SUCCESS);
+			break;
+		}
+	}
+	delete p1;
+	return 0;
+}
