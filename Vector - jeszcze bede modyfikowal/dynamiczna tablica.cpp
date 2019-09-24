a24d8901e5f73b44791e26e66bec911ebbbcc66c
knifer0 <Wojtek_430@interia.pl>
2019-09-24 18:44:07 +0200

    Create dynamiczna tablica.cpp

---
+++ Vector - jeszcze bede modyfikowal/dynamiczna tablica.cpp
@@ -0,0 +1,18 @@
+﻿#include "array.h"
+#include <iostream>
+#include <vector>
+int main(void) {
+	using std::cout;
+	using std::endl;
+	using std:: cin;
+	My_Vector <double> t;
+	t.push_back(5);
+	t.push_back(3);
+	t.push_back(69);
+	t.pop_back();
+	t.push_back(55);
+	for (int i = 0; i < t.size(); i++) {
+		cout << t[i] << endl;
+	}
+	return 0;
+}
