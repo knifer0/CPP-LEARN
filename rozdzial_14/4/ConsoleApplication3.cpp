89afd675332f8071d665d59552cba9ca5a043876
knifer0 <Wojtek_430@interia.pl>
2019-09-26 08:25:10 +0200

    Create ConsoleApplication3.cpp

---
+++ rozdzial_14/4/ConsoleApplication3.cpp
@@ -0,0 +1,15 @@
+﻿#include "klasy.h"
+#include "ctime"
+int main(){
+	srand(time(NULL));
+	Person obiekt1;
+	Gunslinger obiekt2;
+	PokerPlayer obiekt3;
+	BadDude obiekt4;
+	Person* wskaziniki[4] = { &obiekt1,&obiekt2,&obiekt3,&obiekt4 }; // obiekt4 error
+	for (int i = 0; i < 4; i++) {
+		wskaziniki[i]->Show();
+		std::cout << std::endl;
+	}
+	return 0;
+}
